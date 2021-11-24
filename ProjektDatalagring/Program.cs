1b2ebe66a48f9c508e4f39f594f09c193a078b0e
Klara Bergman <89419634+Kl-B@users.noreply.github.com>
2021-11-24 19:03:38 +0100

    Fortsatt med konsollappen

--- ProjektDatalagring/Program.cs
+++ ProjektDatalagring/Program.cs
@@ -8,52 +8,66 @@ RestaurantDbContext.Seed();
 
 while (true)
 {
-    Console.WriteLine("RestaurantClient Menu");
+    Console.WriteLine("RestaurantClient Menu\n");
     Console.WriteLine("1 See unsold foodpacks");
     Console.WriteLine("2 See sold foodpacks");
-    Console.WriteLine("3 Add foodpack");
+    Console.WriteLine("3 Add foodpack\n");
 
+    Console.WriteLine("Input choice\n");
     var input = Console.ReadLine();
 
     if (input == "1")
     {
+        Console.Clear();
+        Console.WriteLine("Enter Restaurant ID");
 
-        Console.WriteLine("Enter id");
-
-        var choice = Convert.ToInt32(Console.ReadLine());
-
-        foreach (var box in RestaurantClient.UnsoldFoodboxes(choice))
+        try
         {
-            Console.WriteLine($"Pack ID: {box.Id}, " +
-                              $"Pack Category: {box.Category}" +
-                              $"Pack Name: {box.Name}");
+            var choice = Convert.ToInt32(Console.ReadLine());
+            foreach (var box in RestaurantClient.UnsoldFoodboxes(choice))
+            {
+                Console.WriteLine($"Pack ID: {box.Id}, " +
+                                  $"Pack Category: {box.Category}, " +
+                                  $"Pack Name: {box.Name}");
+            }
         }
-
-        Console.ReadLine();
+        catch (Exception e)
+        {
+            Console.WriteLine("Input format not correct, please enter a number");
+        }
+        Console.ReadKey();
     }
 
     if (input == "2")
     {
+        Console.Clear();
+        Console.WriteLine("Enter Restaurant ID");
 
-        Console.WriteLine("Enter id");
-
-        var choice = Convert.ToInt32(Console.ReadLine());
-
-        foreach (var box in RestaurantClient.SoldFoodboxes(choice))
+        try
         {
-            Console.WriteLine($"Pack ID: {box.Id}, " +
-                              $"Pack Name: {box.Name}" +
-                              $"Pack Category: {box.Category}, " +
-                              $"Order ID: {box.Order.Id}, " +
-                              $"Pack Price: {box.Price} SEK");
+            var choice = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var box in RestaurantClient.SoldFoodboxes(choice))
+            {
+                Console.WriteLine($"Pack ID: {box.Id}, " +
+                                  $"Pack Name: {box.Name}, " +
+                                  $"Pack Category: {box.Category}, " +
+                                  $"Order ID: {box.Order.Id}, " +
+                                  $"Pack Price: {box.Price} SEK");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Input format not correct, please enter a number");
         }
 
-        Console.ReadLine();
+        Console.ReadKey();
     }
 
     if (input == "3")
     {
-        Console.WriteLine("Enter id, category, price");
+        Console.Clear();
+        Console.WriteLine("Enter id, category, price, name");
         var input1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
         var id = Convert.ToInt32(input1[0]);
         var category = input1[1];
@@ -63,11 +77,15 @@ while (true)
         var RestaurantClient = new RestaurantClient();
 
         RestaurantClient.AddFoodbox(id, category, price, name);
+        Console.WriteLine("Your restaurant has been added");
+        Console.ReadKey();
     }
 
     else
     {
+        Console.Clear();
         Console.WriteLine("Error input, try again");
+        Console.ReadKey();
     }
 
   
