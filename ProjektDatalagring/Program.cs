67bc23eadcf135548d859a2cea93e7e349e9001d
Klara Bergman <89419634+Kl-B@users.noreply.github.com>
2021-11-24 15:47:21 +0100

    Lagit till Konsollapp

--- ProjektDatalagring/Program.cs
+++ ProjektDatalagring/Program.cs
@@ -5,8 +5,72 @@ RestaurantDbContext ctx = new RestaurantDbContext();
 ctx.Database.EnsureDeleted();
 ctx.Database.EnsureCreated();
 ctx.Seed();
-Console.WriteLine("Deleted, Created, Seeded");
 
-var RestaurantClient = new RestaurantClient();
+while (true)
+{
+    Console.WriteLine("RestaurantClient Menu");
+    Console.WriteLine("1 See unsold foodpacks");
+    Console.WriteLine("2 See sold foodpacks");
+    Console.WriteLine("3 Add foodpack");
 
-//RestaurantClient.AddFoodbox(1, "Beef", 45);
+    var input = Console.ReadLine();
+
+    if (input == "1")
+    {
+        var rest = new RestaurantClient();
+
+        Console.WriteLine("Enter id");
+
+        var choice = Convert.ToInt32(Console.ReadLine());
+
+        foreach (var box in rest.UnsoldFoodboxes(choice))
+        {
+            Console.WriteLine($"Pack ID: {box.Id}, " +
+                              $"Pack Category: {box.Category}");
+        }
+
+        Console.ReadLine();
+    }
+
+    if (input == "2")
+    {
+        var rest = new RestaurantClient();
+
+        Console.WriteLine("Enter id");
+
+        var choice = Convert.ToInt32(Console.ReadLine());
+
+        foreach (var box in rest.SoldFoodboxes(choice))
+        {
+            Console.WriteLine($"Pack ID: {box.Id}, " +
+                              $"Pack Category: {box.Category}, " +
+                              $"Order ID: {box.Order.Id}, " +
+                              $"Pack Price: {box.Price} SEK");
+        }
+
+        Console.ReadLine();
+    }
+
+    if (input == "3")
+    {
+        Console.WriteLine("Enter id, category, price");
+        var input1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var id = Convert.ToInt32(input1[0]);
+        var category = input1[1];
+        var price = Convert.ToInt32(input1[2]);
+
+        var RestaurantClient = new RestaurantClient();
+
+        RestaurantClient.AddFoodbox(id, category, price);
+    }
+
+    else
+    {
+        Console.WriteLine("Error input, try again");
+    }
+
+  
+
+
+    
+}
