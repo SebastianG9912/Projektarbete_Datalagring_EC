f3425e7d44cc1c4872789e33bea3a29c5b01ea20
Jakob Dennryd <jakob.dennryd@learnet.se>
2021-11-29 11:19:32 +0100

    SaveChange error

--- TestProject1/CustomerClientTest.cs
+++ TestProject1/CustomerClientTest.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using Backend;
 using Backend.Clients;
 using Backend.Data;
 using Backend.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestProject1
 {
@@ -21,10 +23,24 @@ namespace TestProject1
         [Fact]
         public void TestPurchase()
         {
+            using var ctx = new RestaurantDbContext();
             var client = new CustomerClient();
             client.PurchaseFoodpack(new List<int>(){3}, 1);
-            Assert.True(client.PurchasedFoodpacks(1).Find(f => f.Id == 2) != null);
 
+            var query = ctx.Orders.Include(o => o.Customer)
+                .Include(o => o.Foodpacks)
+                .Where(o => o.Customer.Id == 1 && o.Foodpacks.Contains(new Foodpack()
+                {
+                    Id = 3,
+                    Category = "Beef",
+                    Price = 70,
+                    Restaurant = new Restaurant()
+                    {
+                        Name = "NiceFood", Location = "Eldsberga", Phone_number = "0701234567"
+                    }
+                }));
+            var order = query.FirstOrDefault();
+            Assert.NotNull(order);
         }
     }
 }
